ea13150aa484640f763bed7c415a02ed9ecb4e37
Alexandra J <ajanin@zeenea.com>
2026-06-09 11:07:35 +0200

    Add github action

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 const eleventyGoogleFonts = require("eleventy-google-fonts");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
+  
   eleventyConfig.addPassthroughCopy({
     "src/assets": "assets",
   });
 
   eleventyConfig.addPlugin(eleventyGoogleFonts);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "dist",
+      output: "_site",
     },
 
     templateFormats: ["njk", "md", "html"],
